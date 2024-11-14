134451d8448e61a05fc687a4980ec3f5913d0a90
student@BarakaK1PC08 <student@BarakaK1PC08.com>
2024-11-14 17:50:10 +0100

    prvi_commit

---
+++ RUAP_LV1/RUAP_LV/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RUAP_LV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pozdrav iz aplikacije od Maja Lesnjakovic");
+        }
+    }
+}
